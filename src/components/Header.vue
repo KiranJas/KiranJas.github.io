955caec39c8395fe16b89534c59b61413c91b56e
Kiran <jasvanee.kiran@gmail.com>
2020-09-25 23:58:47 +0530

    Added tags

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,83 +1,108 @@
 <template>
-    <header class="header">
-    <b-navbar toggleable="lg" type="dark" variant="info" style="padding: 0.5rem 2rem;">
-      <div style="width: 360px;"> 
-          <div class="float-left" style="text"> 
-            <img :src="require('../assets/coffee-icon.png')" style="width: 30px; margin: 10px 12px 10px 10px"/>
-            <!-- <img src="../assets/coffee-icon.png" style="width: 30px; margin: 10px 12px 10px 10px"/> -->
-            <img src="../assets/coffee_with_swift.png" style="height: 40px; margin: 16px 0px 0px 0px"/>
-          </div>
-    </div>
-       
-        <!-- <b-navbar-brand href="#">NavBar</b-navbar-brand> -->
+  <header class="header">
+    <b-navbar
+      toggleable="lg"
+      type="dark"
+      variant="info"
+      style="padding: 0.5rem 2rem"
+    >
+      <div style="width: 360px">
+        <div class="float-left" style="text">
+          <img
+            :src="require('../assets/coffee-icon.png')"
+            style="width: 30px; margin: 10px 12px 10px 10px"
+          />
+          <!-- <img src="../assets/coffee-icon.png" style="width: 30px; margin: 10px 12px 10px 10px"/> -->
+          <img
+            src="../assets/coffee_with_swift.png"
+            style="height: 40px; margin: 16px 0px 0px 0px"
+          />
+        </div>
+      </div>
+
+      <!-- <b-navbar-brand href="#">NavBar</b-navbar-brand> -->
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item to="/" exact exact-active-class="active">HOME</b-nav-item>
-          <b-nav-item to="/about" exact exact-active-class="active">ABOUT</b-nav-item>
+          <b-nav-item to="/about" exact exact-active-class="active"
+            >ABOUT</b-nav-item
+          >
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
 
     <div class="float-left subheader">
-       <div class="float-left headerIcons">
-         <img src="../assets/github-logo.png" class="contactIcon" height="22px" />
-          <img src="../assets/stack-logo.png" class="contactIcon" height="22px" />
-          <img src="../assets/linkedin.png" class="contactIcon" height="22px" />
-          <img src="../assets/mail-logo.png" class="contactIcon" height="22px" />
-          <img src="../assets/twitter-logo.png" class="contactIcon" height="22px"/>
-       </div>
-       <b-button class="float-left tagColor">I am a Button</b-button>
-    </div>
-    </header>
+      <div class="float-left headerIcons">
+        <img
+          src="../assets/github-logo.png"
+          class="contactIcon"
+          height="22px"
+        />
+        <img src="../assets/stack-logo.png" class="contactIcon" height="22px" />
+        <img src="../assets/linkedin.png" class="contactIcon" height="22px" />
+        <img src="../assets/mail-logo.png" class="contactIcon" height="22px" />
+        <img
+          src="../assets/twitter-logo.png"
+          class="contactIcon"
+          height="22px"
+        />
+      </div>
 
-    
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "Header"
-}
+  name: "Header",
+};
 </script>
 
 <style scoped>
 .bg-info {
-    background-color: #087A7E !important;
+  background-color: #087a7e !important;
 }
 
 .subheader {
-    background-color: #087A7E;
-    width: 100%;
-    padding: 0.0rem 2rem;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
+  background-color: #087a7e;
+  width: 100%;
+  padding: 0rem 2rem;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
 }
 
 /* customizing navigation bar gap */
 .headerIcons {
-  margin: 10px 10px 16px 10px; 
-  width: 100%; 
+  margin: 10px 10px 16px 10px;
+  width: 100%;
   text-align: left;
 }
 .contactIcon {
-    margin-bottom: 8px;
-    margin-left: 4px;
-    margin-right: 8px;
+  margin-bottom: 8px;
+  margin-left: 4px;
+  margin-right: 8px;
 }
 
 .header_title {
-    
-    text-align: left; 
-    margin: 0px 10px 2  0px 12px;
-    color: white;
-    font-size: bold;
+  text-align: left;
+  margin: 0px 10px 2 0px 12px;
+  color: white;
+  font-size: bold;
 }
 
 .tagColor {
-    background-color: blue;
-    align-content: left;
-    text-align: left;
-    margin: 0px 0px 20px 10px; 
+  background-color: #d2f6c5;
+  color: black;
+  align-content: left;
+  text-align: left;
+  margin: 0px 0px 20px 10px;
+
+  font-family: "Poppins-Medium", sans-serif;
+  font-size: 0.8rem;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  border-radius: 6px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,24 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <div style="opacity: 0.0">Invisible View</div>
+    <div class="row tags">
+      <!-- #d2f6c5 -->
+      <b-button style="background-color: #222831;" 
+      class="float-left tagColor">News</b-button>
+      <b-button style="background-color: #0085ba; margin-left: 10px;" 
+      class="float-left tagColor">SwiftUI</b-button>
+      <b-button style="background-color: #ff4b5c; margin-left: 10px;" 
+      class="float-left tagColor">Swift</b-button>
+      <b-button style="background-color: #81b214; margin-left: 10px;" 
+      class="float-left tagColor">UIKit</b-button>
+      <b-button style="background-color: #557571; margin-left: 10px;" 
+      class="float-left tagColor">Xcode</b-button>
+      <b-button style="background-color: #ba6b57; margin-left: 10px;" 
+      class="float-left tagColor">Tips</b-button>
+      <b-button style="background-color: #776d8a; margin-left: 10px;" 
+      class="float-left tagColor">Data Structure</b-button>
+    </div>
+
     <TitleDeclarationCom/>
     <BlogBriefCom/>
   </div>
@@ -21,5 +39,27 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.tags {
+    margin-left: 10%;
+    margin-right: 10%;
+    width: 100%;
+    /* margin-top: 80px; */
+    text-align: left;
+}
+
+.tagColor {
+  background-color: #d2f6c5;
+  color: white;
+  align-content: left;
+  text-align: left;
+
+  font-family: "Poppins-Medium", sans-serif;
+  font-size: 0.8rem;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  border-radius: 6px;
+  border-width: 0px;
+  margin-bottom: 30px;
+}
 </style>
